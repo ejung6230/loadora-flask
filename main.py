f5a319dffc2aa7ca78f3a0836b2360d8afa096d9
ejung6230 <ejung6230@naver.com>
2025-08-18 21:32:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -601,7 +601,7 @@ def korlark_summary():
 
         if request.method=="POST":
             return jsonify({"version":"2.0","template":{"outputs":[{"simpleText":{"text":summary_text}}]}})
-        return current_data
+        return all_data
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -622,6 +622,7 @@ def korlark_proxy():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
