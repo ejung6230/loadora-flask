588d64161e55d6d6d2509de6e0b0339f1348db7f
ejung6230 <ejung6230@naver.com>
2025-09-02 13:40:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -963,7 +963,7 @@ def fallback():
                         expedition_text = f"◕ᴗ◕🌸\n❛{expedition_char_name}❜ 님의 원정대 정보를 알려드릴게요.\n\n"
                         for server, chars in organized_chars.items():
                             chars.sort(key=lambda x: x['ItemAvgLevel'], reverse=True)
-                            expedition_text += f"❙ {server} ({len(chars)}개)\n"
+                            expedition_text += f"❙ {server} ({len(chars)})\n"
 
                             # 출력 부분 수정
                             for c in chars:
@@ -2194,6 +2194,7 @@ def korlark_proxy():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
