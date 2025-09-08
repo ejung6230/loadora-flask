69cd75257388b05223d5914b13928549179006a8
ejung6230 <ejung6230@naver.com>
2025-09-08 11:50:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1602,7 +1602,7 @@ def fallback():
                                 "buttons": [
                                     {
                                         "label": "원정대 정보",
-                                        "highlight": false,
+                                        "highlight": False,
                                         "action": "message",
                                         "extra": {},
                                         "messageText": f".원정대 {info_char_name}"
@@ -2623,6 +2623,7 @@ def korlark_proxy():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
