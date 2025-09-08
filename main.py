532c4d962b9a68d855e66460e81da83f1d651ee2
ejung6230 <ejung6230@naver.com>
2025-09-08 11:23:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1581,12 +1581,12 @@ def fallback():
                                 ]
                             }
                         },
-                        {
-                            "simpleText": {
-                                "text": preview_text,
-                                "extra": {}
-                            }
-                        },
+                        # {
+                        #     "simpleText": {
+                        #         "text": preview_text,
+                        #         "extra": {}
+                        #     }
+                        # },
                     ]
                 else:
                     # 데이터 없으면 텍스트 카드만
@@ -2610,6 +2610,7 @@ def korlark_proxy():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
