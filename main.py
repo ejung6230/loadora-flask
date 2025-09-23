35b6640ba977cf80339cf4b7c7f9121c02db422f
ejung6230 <ejung6230@naver.com>
2025-09-23 19:11:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1867,9 +1867,10 @@ PVP: {pvp_grade_name}
                         value = element.get("value", "")
                         if isinstance(value, str):
                             clean_tooltip = clean_html_tooltip(value)
+                            logger.info("아크패시브보기%s", clean_tooltip)
                             if any(pattern in clean_tooltip for pattern in patterns):
                                 summary_text = summarize_synergy_full(clean_tooltip)
-                                logger.info("아크패시브보기%s", clean_tooltip)
+                                
                                 synergy_skills.append({
                                     "Name": clean_name,
                                     "Tooltip": clean_tooltip,
