f0a596582c24c2117590ec9e5fd274ffbb85111e
ejung6230 <ejung6230@naver.com>
2025-09-29 13:54:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1709,12 +1709,11 @@ def fallback():
         
             lines = []
         
-            if item_name:
-                lines.append(f"◕ᴗ◕🌸\n'{item_name}' 유물 각인서 가격을 알려드릴게요\n")
-            else:
-                lines.append("◕ᴗ◕🌸\n상위 10개의 유물 각인서 가격을 알려드릴게요\n")
-
             data_items = data.get("Items", [])
+            data_cnt = data.get("TotalCount", 0)
+            
+            lines.append(f"◕ᴗ◕🌸\n{data_cnt}개의 유물 각인서 가격을 알려드릴게요\n")
+            
             if data_items:
                 for entry in data_items:
                     name = entry.get('Name', '').replace('유물 ', '').replace(' 각인서', '')
@@ -1734,7 +1733,7 @@ def fallback():
                     else:
                         change_text = "N/A"
                     
-                    lines.append(f"❙ {name} : {current_price:,} ({change_text})")
+                    lines.append(f"❙ {current_price:,}🪙 : {name} ({change_text})")
             else:
                 lines.append("❙ 조회된 유물 각인서가 없습니다. 이름을 다시 확인해주세요.")
             
@@ -3005,6 +3004,7 @@ def korlark_proxy():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
