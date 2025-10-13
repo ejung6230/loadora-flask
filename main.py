7612c2f245b942fe834f6e03c5fdff8daf874f5b
ejung6230 <ejung6230@naver.com>
2025-10-13 17:40:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3982,8 +3982,9 @@ def korlark_proxy():
 
 # ------------------ 실행 ------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
